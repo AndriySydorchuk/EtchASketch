b1be2e90d7e8e46991ed464cc565ba80d789cec4
Andriy Sydorchuk <programmers130822@gmail.com>
2025-08-10 00:40:46 +0300

    Set up a 'hover' effect for grid items
    When cursor over an item it's background color changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gridContainer = document.querySelector('.grid');
 
 const gridItems = [];
 
-const GRID_SIZE = 16;
+const GRID_SIZE = 64;
 
 for (let i = 0; i < GRID_SIZE; i++) {
     const gridItem = document.createElement('div');
@@ -14,15 +14,18 @@ for (let i = 0; i < GRID_SIZE; i++) {
     gridItem.style.width = `${gridItemSize}px`;
     gridItem.style.height = 'auto';
     
-    gridItem.style.backgroundColor = 'lightgray';
     gridItem.style.border = '1px solid black';
 
     gridItems.push(gridItem);
     gridContainer.appendChild(gridItem);
 }
 
-console.log(calcGridItemSize(gridContainer.offsetWidth));
-
 function calcGridItemSize(gridWidth) {
     return gridWidth / Math.sqrt(GRID_SIZE);
 }
+
+gridItems.forEach(item => item.addEventListener('mouseover', e => {
+    if(item.style.backgroundColor !== 'black') {
+        item.style.backgroundColor = 'black';
+    }
+}));
